ffcbaaf9e9cabda07061452c7ce47790e95d4ce8
mschwettmann1 <marc-schwettmann@web.de>
2018-06-03 15:49:01 +0200

    readPages Methode ergänzt und savePDF erweitert

--- src/main/java/PDFReader.java
+++ src/main/java/PDFReader.java
@@ -15,30 +15,42 @@ public class PDFReader {
 
     public void savePDF(String text)throws IOException{
         document = new PDDocument();
+        int start=0;
+        int line=1;
+
         File file;
-        PDPage page1 = new PDPage();
         String OS =System.getProperty("os.name");
-        
-        if(OS.substring(0,7).equals("Windows")){
-            file=new File("C:/Windows/Fonts/Arial.ttf");
+
+        if(OS.substring(0,5).equals("Linux")){
+            file=new File("/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL_R.ttf");
         }
-        else if(OS.substring(0,5).equals("Linux")){
-            file=new File("/usr/share/fonts");
+        else if(OS.substring(0,7).equals("Windows")){
+            file=new File("C:/Windows/Fonts/Arial.ttf");
         }
         else{
             file=new File("/Library/Fonts/Arial.ttf");
         }
-        document.addPage( page1 );
+
+        PDPage page1 = new PDPage();
+        document.addPage(page1);
 
         PDPageContentStream contentStream = new PDPageContentStream(document, page1);
         contentStream.beginText();
         contentStream.setFont(PDType0Font.load(document, file), 12);
         contentStream.newLineAtOffset(25, 750);
-        contentStream.showText(text);
+        contentStream.setLeading(14.5f);
+        for(int i=65; i<text.length();i=i+65) {
+            contentStream.showText(text.substring(start, i));
+            contentStream.newLine();
+            line++;
+
+            start = start + 65;
+        }
+        contentStream.showText(text.substring(start, text.length()));
+
         contentStream.endText();
         contentStream.close();
-
-        document.save(System.getProperty("user.dir")+"/Summary.pdf");
+        document.save(System.getProperty("user.dir") + "/Summary.pdf");
         document.close();
     }
 
@@ -51,11 +63,23 @@ public class PDFReader {
         return text;
     }
 
+    public String readPages(File doc,int start,int end)throws IOException{
+        document=PDDocument.load(doc);
+
+        PDFTextStripper reader = new PDFTextStripper();
+        reader.setStartPage(start);
+        reader.setEndPage(end);
+        String pageText = reader.getText(document);
+
+        System.out.println(pageText);
+        return pageText;
+    }
+
     public static void main(String[] args) {
         PDFReader reader = new PDFReader();
         try {
-            //File file=new File("C:/Users/Marc/Desktop/apple.pdf");
-            //reader.readPDF(file);
+            //File file=new File("C:/Users/Marc/Desktop/Unbenannt 1.pdf");
+            //reader.readPages(file,1,2);
             reader.savePDF("ABC");
         }
         catch (IOException e) {
