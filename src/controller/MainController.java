acf5f5224c45d5a77e0008146efa7653cb1c61d0
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2018-06-07 20:59:51 +0200

    împl fuctions

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,31 +1,27 @@
 package controller;
 import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainController extends Application {
-    public static void main(String[] args) {
+	static Stage ps;
+	public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
-
-
+    public void start(Stage primaryStage){
+    	ps=primaryStage;
         FXMLLoader loader =new FXMLLoader();
         loader.setLocation(getClass().getResource("summarizerMain.fxml"));
         try {
-			primaryStage.setScene(loader.load());
+        	Scene scene = new Scene (loader.load());
+			primaryStage.setScene(scene);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
         primaryStage.show();
