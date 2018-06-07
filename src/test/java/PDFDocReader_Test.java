8603cc51eabde48b6212283052dd37bcf586342d
Jonas Posselt <jonas.posselt@web.de>
2018-06-07 17:31:35 +0200

    Refactoring tests

--- src/test/java/PDFDocReader_Test.java
+++ src/test/java/PDFDocReader_Test.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PDFDocReader_Test {
 
     private PDFDocReader pdfReader;
-    private String noExtensionPath, unknownExtensionPath, helloPdfPath;
+    private String noExtensionPath, unknownExtensionPath, noFilePath, helloPdfPath;
 
     @BeforeEach
     void initEach() {
@@ -16,14 +16,32 @@ class PDFDocReader_Test {
 
         noExtensionPath = "./src/test/resources/pdf";
         unknownExtensionPath = "./src/test/resources/pdf/test.dat";
+        noFilePath = "./src/test/resources/pdf/noFile.pdf\"";
         helloPdfPath = "./src/test/resources/pdf/HelloWorld.pdf";
     }
 
     @Test
-    void readDocument() {
+    void test_readDocument_null() {
         assertThrows(NullPointerException.class, () -> pdfReader.readDocument(null));
+    }
+
+    @Test
+    void test_readDocument_noExtension() {
         assertThrows(IllegalArgumentException.class, () -> pdfReader.readDocument(noExtensionPath));
+    }
+
+    @Test
+    void test_readDocument_unknownExtension() {
         assertThrows(IllegalArgumentException.class, () -> pdfReader.readDocument(unknownExtensionPath));
+    }
+
+    @Test
+    void test_readDocument_noFile() {
+        assertThrows(IllegalArgumentException.class, () -> pdfReader.readDocument(noFilePath));
+    }
+
+    @Test
+    void test_readDocument_normal() {
         try {
             assertEquals("hello world", pdfReader.readDocument(helloPdfPath));
         } catch (IOException e) {
--- src/test/java/WordDocReader_Test.java
+++ src/test/java/WordDocReader_Test.java
@@ -9,33 +9,49 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordDocReader_Test {
 
     private WordDocReader wordReader;
-    private File docFile, docxFile, helloDoc, helloDocx;
-    private String docPath, docxPath, noExtensionPath, unknownExtensionPath, helloDocPath, helloDocxPath;
+    private File noDocFile, noDocxFile, helloDoc, helloDocx;
+    private String noDocPath, noDocxPath, noExtensionPath, unknownExtensionPath, helloDocPath, helloDocxPath;
 
 
     @BeforeEach
     void initEach() {
         wordReader = new WordDocReader();
-        docPath = "./src/test/resources/word/test.doc";
-        docxPath = "./src/test/resources/word/test.docx";
+        noDocPath = "./src/test/resources/word/test.doc";
+        noDocxPath = "./src/test/resources/word/test.docx";
         noExtensionPath = "./src/test/resources/word";
         unknownExtensionPath = "./src/test/resources/word/test.dat";
         helloDocPath = "./src/test/resources/word/Hello.doc";
         helloDocxPath = "./src/test/resources/word/Hello.docx";
 
-        docFile = new File(docPath);
-        docxFile = new File(docxPath);
+        noDocFile = new File(noDocPath);
+        noDocxFile = new File(noDocxPath);
         helloDoc = new File(helloDocPath);
         helloDocx = new File(helloDocxPath);
     }
 
     @Test
-    void readDocument() {
+    void test_readDocument_null() {
         assertThrows(NullPointerException.class, () -> wordReader.readDocument(null));
+    }
+
+    @Test
+    void test_readDocument_noExtension() {
         assertThrows(IllegalArgumentException.class, () -> wordReader.readDocument(noExtensionPath));
+    }
+
+    @Test
+    void test_readDocument_unknownExtension() {
         assertThrows(IllegalArgumentException.class, () -> wordReader.readDocument(unknownExtensionPath));
-        assertThrows(IOException.class, () -> wordReader.readDocument(docPath));
-        assertThrows(IOException.class, () -> wordReader.readDocument(docxPath));
+    }
+
+    @Test
+    void test_readDocument_noFile() {
+        assertThrows(IOException.class, () -> wordReader.readDocument(noDocPath));
+        assertThrows(IOException.class, () -> wordReader.readDocument(noDocxPath));
+    }
+
+    @Test
+    void test_readDocument_normal() {
         try {
             assertEquals("Hello World", wordReader.readDocument(helloDocPath));
             assertEquals("Hello World", wordReader.readDocument(helloDocxPath));
@@ -45,11 +61,22 @@ class WordDocReader_Test {
     }
 
     @Test
-    void readDocFile() {
+    void test_readDocFile_null() {
         assertThrows(NullPointerException.class, () -> wordReader.readDocFile(null));
-        assertThrows(IOException.class, () -> wordReader.readDocFile(docFile));
-        assertThrows(IOException.class, () -> wordReader.readDocFile(docxFile));
+    }
+
+    @Test
+    void test_readDocFile_noFile() {
+        assertThrows(IOException.class, () -> wordReader.readDocFile(noDocFile));
+    }
+
+    @Test
+    void test_readDocFile_wrongExtension() {
         assertThrows(IllegalArgumentException.class, () -> wordReader.readDocFile(helloDocx));
+    }
+
+    @Test
+    void test_readDocFile_normal() {
         try {
             assertEquals("Hello World", wordReader.readDocFile(helloDoc));
         } catch (IOException e) {
@@ -58,11 +85,22 @@ class WordDocReader_Test {
     }
 
     @Test
-    void readDocxFile() {
+    void test_readDocxFile_null() {
         assertThrows(NullPointerException.class, () -> wordReader.readDocxFile(null));
-        assertThrows(IOException.class, () -> wordReader.readDocxFile(docFile));
-        assertThrows(IOException.class, () -> wordReader.readDocxFile(docxFile));
+    }
+
+    @Test
+    void test_readDocxFile_noFile() {
+        assertThrows(IOException.class, () -> wordReader.readDocxFile(noDocxFile));
+    }
+
+    @Test
+    void test_readDocxFile_wrongExtension() {
         assertThrows(IllegalArgumentException.class, () -> wordReader.readDocxFile(helloDoc));
+    }
+
+    @Test
+    void test_readDocxFile_normal() {
         try {
             assertEquals("Hello World", wordReader.readDocxFile(helloDocx));
         } catch (IOException e) {
