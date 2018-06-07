b693cdb24d35ed6ba764dc9b7e5edf421f43c757
Jonas Posselt <jonas.posselt@web.de>
2018-06-07 17:10:19 +0200

    Check file extension and tests

--- src/main/java/PDFDocReader.java
+++ src/main/java/PDFDocReader.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+
+import org.apache.commons.io.FilenameUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
@@ -7,11 +9,19 @@ public class PDFDocReader implements IDocumentReader {
     @Override
     public String readDocument(String path) throws IOException {
         File file = new File(path);
-        try (PDDocument document = PDDocument.load(file)) {
-            // Create TextStripper
-            PDFTextStripper pdfStripper = new PDFTextStripper();
-            // Strip text from document (ignore the formatting)
-            return pdfStripper.getText(document);
+
+        String fileExtension = FilenameUtils.getExtension(file.getName());
+        if (fileExtension.equalsIgnoreCase("pdf")) {
+            try (PDDocument document = PDDocument.load(file)) {
+                // Create TextStripper
+                PDFTextStripper pdfStripper = new PDFTextStripper();
+                // Strip text from document (ignore the formatting)
+                return pdfStripper.getText(document).trim();
+            }
+        } else {
+            throw new IllegalArgumentException("File type not supported: " + fileExtension);
         }
+
+
     }
 }
--- src/test/java/PDFDocReader_Test.java
+++ src/test/java/PDFDocReader_Test.java
@@ -1,20 +1,33 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PDFDocReader_Test {
 
     private PDFDocReader pdfReader;
+    private String noExtensionPath, unknownExtensionPath, helloPdfPath;
 
     @BeforeEach
     void initEach() {
         pdfReader = new PDFDocReader();
-    }
 
+        noExtensionPath = "./src/test/resources/pdf";
+        unknownExtensionPath = "./src/test/resources/pdf/test.dat";
+        helloPdfPath = "./src/test/resources/pdf/HelloWorld.pdf";
+    }
 
     @Test
     void readDocument() {
         assertThrows(NullPointerException.class, () -> pdfReader.readDocument(null));
+        assertThrows(IllegalArgumentException.class, () -> pdfReader.readDocument(noExtensionPath));
+        assertThrows(IllegalArgumentException.class, () -> pdfReader.readDocument(unknownExtensionPath));
+        try {
+            assertEquals("hello world", pdfReader.readDocument(helloPdfPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
